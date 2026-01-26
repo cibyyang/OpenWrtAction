078fcc3560ff0c74131d0164d0ef92909943be70
cibyyang <cibyyang@users.noreply.github.com>
2026-01-26 17:30:25 +0800

    Update lean_custom_feeds_and_packages.sh

--- diy_script/lean_diy/lean_custom_feeds_and_packages.sh
+++ diy_script/lean_diy/lean_custom_feeds_and_packages.sh
@@ -7,6 +7,7 @@ export repos=(
   "src-git helloworld https://github.com/fw876/helloworld;master"
   "src-git OpenClash https://github.com/vernesong/OpenClash;master"
   "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main"
+  "src-git qmodem https://github.com/FUjr/QModem.git;main"
 )
 
 clone_custom_packages () {
